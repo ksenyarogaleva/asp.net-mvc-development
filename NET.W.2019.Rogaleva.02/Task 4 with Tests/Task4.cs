38280197bfea2f89b65b2e9b5b6e76e35dddd147
ksenyarogaleva <55576114+ksenyarogaleva@users.noreply.github.com>
2019-11-17 11:46:56 +0300

    Add files via upload

---
+++ NET.W.2019.Rogaleva.02/Task 4 with Tests/Task4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tasks
+{
+
+    /// <summary>
+    /// The main<c>Task4</c> class.
+    /// Contains methods for filtering list of integer elements depending on teh condition.
+    /// </summary>
+    public class Task4
+    {
+
+        /// <summary>
+        /// Filters list.
+        /// </summary>
+        /// <param name="list">List of integer elements.</param>
+        /// <param name="number">An integer.</param>
+        /// <returns>List with elements that contain the given digit.</returns>
+        public static List<int> FilterDigit(List<int> list, int number)
+        {
+            List<int> filteredList = new List<int>();
+            for (int i = 0; i < list.ToArray().Length; i++)
+            {
+                bool check = true;
+                if (list[i] != number && list[i] % 10 != number && list[i] / 10 != number)
+                    check = false;
+                if (check && !filteredList.Contains(list[i]))
+                    filteredList.Add(list[i]);
+            }
+
+            return filteredList;
+        }
+    }
+}
