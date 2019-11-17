81b3b8eb1a921613d20fc84a79b712a96753132d
ksenyarogaleva <55576114+ksenyarogaleva@users.noreply.github.com>
2019-11-17 12:03:47 +0300

    Add files via upload

---
+++ NET.W.2019.Rogaleva.02/Task5 with Tests/Task5.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Tasks
+{
+
+    /// <summary>
+    /// The main <c>Task5</c> class.
+    /// Contains realization of Newton's method of finding Nth root.
+    /// </summary>
+    public class Task5
+    {
+        /// <summary>
+        /// Finds Nth root.
+        /// </summary>
+        /// <param name="number">Number to find its root.</param>
+        /// <param name="rootDegree">Root degree of a number.</param>
+        /// <param name="accuracy">Accuracy.</param>
+        /// <returns>Nth root of a number that was found.</returns>
+        public static double FindNthRoot(double number, int rootDegree, double accuracy)
+        {
+            CheckInput(number, rootDegree, accuracy);
+            double root = number / rootDegree;
+            double resultRoot = NewtonMethod(rootDegree, number, root);
+
+            while (Math.Abs(resultRoot - root) > accuracy)
+            {
+                root = resultRoot;
+                resultRoot = NewtonMethod(rootDegree, number, root);
+            }
+            return resultRoot;
+        }
+
+        /// <summary>
+        /// Checks input parameters.
+        /// </summary>
+        /// <param name="n">Number.</param>
+        /// <param name="i">Root degree of a number.</param>
+        /// <param name="acc">Accuracy.</param>
+        public static void CheckInput(double n, int i, double acc)
+        {
+            if (acc < 0) throw new ArgumentOutOfRangeException("Accuracy can't be < 0");
+            if (i <= 0 || n <= 0 & i % 2 == 0) throw new ArgumentOutOfRangeException("Number can't be < 0");
+        }
+
+        /// <summary>
+        /// Applies Newton's formula.
+        /// </summary>
+        /// <param name="degree">Root degree of a number.</param>
+        /// <param name="num">Number to find its root.</param>
+        /// <param name="root">Temporary value of a root.</param>
+        /// <returns></returns>
+        public static double NewtonMethod(int degree, double num, double root)
+        {
+            return 1.0 / degree * ((degree - 1) * root + num / Math.Pow(root, degree - 1));
+        }
+    }
+}
---
+++ NET.W.2019.Rogaleva.02/Task5 with Tests/Task5Tests.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks;
+using System;
+
+namespace Tasks.Tests
+{
+    [TestClass()]
+    public class Task5Tests
+    {
+        [TestMethod()]
+        public void FindNthRootTestCase1()
+        {
+            //add
+            double testNumber = 1.0;
+            int testDegree = 5;
+            double precision = 0.0001;
+            double expected = 1.0;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase2()
+        {
+            //add
+            double testNumber = 8.0;
+            int testDegree = 3;
+            double precision = 0.0001;
+            double expected = 2.0;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase3()
+        {
+            //add
+            double testNumber = 0.001;
+            int testDegree = 3;
+            double precision = 0.0001;
+            double expected = 0.1;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase4()
+        {
+            //add
+            double testNumber = 0.04100625;
+            int testDegree = 4;
+            double precision = 0.0001;
+            double expected = 0.45;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase5()
+        {
+            //add
+            double testNumber = 0.0279936;
+            int testDegree = 7;
+            double precision = 0.0001;
+            double expected = 0.6;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase6()
+        {
+            //add
+            double testNumber = 0.0081;
+            int testDegree = 4;
+            double precision = 0.1;
+            double expected = 0.3;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase7()
+        {
+            //add
+            double testNumber = -0.008;
+            int testDegree = 3;
+            double precision = 0.1;
+            double expected = -0.2;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+        }
+
+        [TestMethod()]
+        public void FindNthRootTestCase8()
+        {
+            //add
+            double testNumber = 0.004241979;
+            int testDegree = 9;
+            double precision = 0.00000001;
+            double expected = 0.545;
+
+            //act
+            double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
+
+            //assert
+            Assert.AreEqual(expected, actual, 1);
+
+        }
+        [TestMethod()]
+
+        public void FindNthRootTest_ExceptionCase1()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => (Task5.FindNthRoot(8.0, 15, -0.6)));
+        }
+
+        [TestMethod()]
+
+        public void FindNthRootTest_ExceptionCase2()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => (Task5.FindNthRoot(8.0, 15, -7)));
+        }
+    }
+}
