3149802083316d15e7db2bf92d1f0011c8cfc55a
ksenyarogaleva <55576114+ksenyarogaleva@users.noreply.github.com>
2019-11-20 14:18:20 +0300

    Update Task5Tests.cs

--- NET.W.2019.Rogaleva.02/Task5 with Tests/Task5Tests.cs
+++ NET.W.2019.Rogaleva.02/Task5 with Tests/Task5Tests.cs
@@ -10,15 +10,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase1()
         {
-            //add
+            //arrange
             double testNumber = 1.0;
             int testDegree = 5;
             double precision = 0.0001;
             double expected = 1.0;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -26,15 +24,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase2()
         {
-            //add
+            //arrange
             double testNumber = 8.0;
             int testDegree = 3;
             double precision = 0.0001;
             double expected = 2.0;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -42,15 +38,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase3()
         {
-            //add
+            //arrange
             double testNumber = 0.001;
             int testDegree = 3;
             double precision = 0.0001;
             double expected = 0.1;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -58,15 +52,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase4()
         {
-            //add
+            //arrange
             double testNumber = 0.04100625;
             int testDegree = 4;
             double precision = 0.0001;
             double expected = 0.45;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -74,15 +66,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase5()
         {
-            //add
+            //arrange
             double testNumber = 0.0279936;
             int testDegree = 7;
             double precision = 0.0001;
             double expected = 0.6;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -90,15 +80,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase6()
         {
-            //add
+            //arrange
             double testNumber = 0.0081;
             int testDegree = 4;
             double precision = 0.1;
             double expected = 0.3;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -106,15 +94,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase7()
         {
-            //add
+            //arrange
             double testNumber = -0.008;
             int testDegree = 3;
             double precision = 0.1;
             double expected = -0.2;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
         }
@@ -122,15 +108,13 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FindNthRootTestCase8()
         {
-            //add
+            //arrange
             double testNumber = 0.004241979;
             int testDegree = 9;
             double precision = 0.00000001;
             double expected = 0.545;
-
             //act
             double actual = Task5.FindNthRoot(testNumber, testDegree, precision);
-
             //assert
             Assert.AreEqual(expected, actual, 1);
 
