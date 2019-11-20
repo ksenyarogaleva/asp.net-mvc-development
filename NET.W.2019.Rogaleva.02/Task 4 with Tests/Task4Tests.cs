61735732e132a365613da4d98a8c828300af1c68
ksenyarogaleva <55576114+ksenyarogaleva@users.noreply.github.com>
2019-11-20 14:16:50 +0300

    Update Task4Tests.cs

--- NET.W.2019.Rogaleva.02/Task 4 with Tests/Task4Tests.cs
+++ NET.W.2019.Rogaleva.02/Task 4 with Tests/Task4Tests.cs
@@ -12,7 +12,7 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FilterDigitTest1()
         {
-            //add
+            //arrange
             List<int> testList = new List<int>() { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 };
             int testNumber = 7;
             List<int> expected = new List<int>() { 7, 70, 17 };
@@ -26,7 +26,7 @@ namespace Tasks.Tests
         [TestMethod()]
         public void FilterDigitTest2()
         {
-            //add
+            //arrange
             List<int> testList = new List<int>() { 33, 71, 19, 32, 4, 9, 13, 8, 6, 7, 11, 91, 0 };
             int testNumber = 1;
             List<int> expected = new List<int>() { 71, 19, 13, 11, 91 };
